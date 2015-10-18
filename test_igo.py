7b6d59806680f8fff887d89239db8e49e087e624
Yukich <june.ymd@gmail.com>
2015-10-18 13:18:10 +0900

    add igo-python

--- test_igo.py
+++ test_igo.py
@@ -9,7 +9,7 @@ DIC_DIR = '/home/yamajun/workspace/tmp/igo_ipadic'
 if __name__ == '__main__':
 
 
-    text = u"本日は晴天なり"
+    text = u"今日はとても良い天気ですね"
     tagger = igo.tagger.Tagger(DIC_DIR)
 
     words = tagger.parse(text)
