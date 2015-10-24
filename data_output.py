ce4dc46ea8fbcdbc8127aa2393d2c41d3f0937a9
Yukich <june.ymd@gmail.com>
2015-10-25 00:07:58 +0900

    prog

---
+++ data_output.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+import time
+import argparse
+
+import pandas as pd
+import numpy as np
+import six.moves.cPickle as pickle
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data', '-d', default='result/genre_words.out')
+    args = parser.parse_args()
+
+
+    genres = None
+    genre_words = None
+
+    with open('result/genres_only_b.out', 'rb') as f:
+        genres = pickle.load(f)
+
+    with open(arg.data, 'rb') as f:
+        genre_words = pickle.load(f)
+
+
+
+    for genre_id, words in sorted(genre_words.items(), key=lambda x:len(x[1]), reverse=True):
+        genre_name = genres[genre_id]
+        print(genre_name)
+        for word, num in sorted(words.items(), key=lambda x:x[1], reverse=True)[:20]:
+            print(' {0}\t{1}'.format(word, num))
