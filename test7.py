f6534cd59b93692776850e619788bc6894e06c42
Yukich <june.ymd@gmail.com>
2015-10-24 21:37:30 +0900

    fix

--- test7.py
+++ test7.py
@@ -52,37 +52,32 @@ if __name__ == '__main__':
             print("query :", sql)
             cursor.execute(sql)
             print("query complete")
+            cur_length = len(cursor)
             count = 0
             for row in cursor:
                 genre_name = str(row[0])
-                print("start: {0} : {1}".format(count, genre_name))
-                print(row[1])
                 morphs = igo_parse(row[1])
                 morphs_filtered = [x[7] for x in morphs if x[1] in ["名詞", "動詞"]]
                 if not genre_name in genre_words:
                     genre_words[genre_name] = {}
 
-                print(morphs_filtered)
                 for morph in morphs_filtered:
                     if morph in genre_words[genre_name]:
                         genre_words[genre_name][morph] += 1
                     else:
                         genre_words[genre_name][morph] = 1
 
-                print("end: {0} : {1}".format(count, row[0]))
+                print("end: {0}/{1} : {1}".format(count, cur_length, row[0]))
                 count += 1
 
+    except:
+        print("error")
 
     finally:
         connection.close()
 
-    print('length_n', len(genres))
 
 
-    pickle.dump(genre_words, open('result/genres_b.out', 'wb'), -1)
+    pickle.dump(genre_words, open('result/genre_words_b.out', 'wb'), -1)
 
-
-    for key, value in sorted(genre_words.items(), key=lambda x:len(x[1]), reverse=True):
-        print(key)
-        for key1, value1 in sorted(value.items(), key=lambda x:x[1], reverse=True):
-            print("-\t{0}\t{1}".format(key1, value1))
+    print("complete!")
