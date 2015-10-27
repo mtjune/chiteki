bdce5630c7991e17abfa925b957f6dcbba34f335
Yukich <june.ymd@gmail.com>
2015-10-27 16:08:02 +0900

    prog

--- category_get_vocab_all_filter.py
+++ category_get_vocab_all_filter.py
@@ -32,8 +32,10 @@ if __name__ == '__main__':
                                  cursorclass=pymysql.cursors.SSCursor)
 
 
+    FILTER_NUM = 9
+
     vocab_set = set()
-    vocab_set_one = set()
+    vocab_count = {}
 
     count_invalid = 0
 
@@ -46,10 +48,18 @@ if __name__ == '__main__':
         for row in cursor:
             title = row[0]
             n_words = igo_parse(title)
+            n_words_set = set(n_words)
 
             flag_invalid = True
-            for n_word in n_words:
-                vocab_set.add(n_word)
+            for n_word in n_words_set:
+                if n_word not in vocab_set:
+                    if n_word in vovab_count:
+                        vocab_count[n_word] += 1
+                        if vocab_count[n_word] > 4:
+                            vocab_set.add(n_word)
+                            del vocab_count[n_word]
+                    else:
+                        vocab_count = 1
                 flag_invalid = False
 
             if flag_invalid:
@@ -61,6 +71,7 @@ if __name__ == '__main__':
 
 
     print('end all : vocab size : {}'.format(len(vocab_set)))
+    print('drop by filter word size : {}'.format(len(vocab_count)))
 
     vocab = {}
     count = 0
@@ -70,7 +81,7 @@ if __name__ == '__main__':
 
     print('{} == {}'.format(len(vocab_set), len(vocab)))
 
-    with open('result/category_vocab_all_b.out', 'wb') as f:
+    with open('result/category_vocab_all_filter_b.out', 'wb') as f:
         pickle.dump(vocab, f, -1)
 
     print('complete!')
