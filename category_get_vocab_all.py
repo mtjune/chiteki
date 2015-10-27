f950d06a26fd0f760735f1bff136de8ca4fefe7c
Yukich <june.ymd@gmail.com>
2015-10-27 14:13:54 +0900

    prog

---
+++ category_get_vocab_all.py
@@ -0,0 +1,74 @@
+import pymysql
+import yaml
+
+import pandas as pd
+import numpy as np
+import six.moves.cPickle as pickle
+
+import igo
+
+DIC_DIR = "/home/yamajun/workspace/tmp/igo_ipadic"
+tagger = igo.tagger.Tagger(DIC_DIR)
+def igo_parse(text):
+    words = tagger.parse(text)
+    outputs = [word.surface for word in words if word.feature.split(',')[0] == '名詞']
+    return outputs
+
+
+
+
+if __name__ == '__main__':
+
+    setting = None
+    with open('mysql_setting.yml', 'r') as f:
+        setting = yaml.load(f)
+
+
+    connection = pymysql.connect(host=setting['host'],
+                                 user=setting['user'],
+                                 password=setting['password'],
+                                 db='rakuten_recipe',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.SSCursor)
+
+
+    vocab_set = set()
+
+    count_invalid = 0
+
+    with connection.cursor() as cursor:
+        sql = "select title from recipes;"
+
+        cursor.execute(sql)
+
+        count = 0
+        for row in cursor:
+            title = row[0]
+            n_words = igo_parse(title)
+
+            flag_invalid = True
+            for n_word in n_words:
+                vocab_set.add(n_word)
+                flag_invalid = False
+
+            if flag_invalid:
+                count_invalid += 1
+
+            if count % 10000 == 0:
+                print('end {} : vocab size : {}'.format(count, len(vocab_set)))
+
+
+    print('end all : vocab size : {}'.format(len(vocab_set)))
+
+    vocab = {}
+    count = 0
+    for word in vocab_set:
+        vocab[word] = count
+        count += 1
+
+    print('{} == {}'.format(len(vocab_set), len(vocab)))
+
+    with open('result/category_vocab_all_b.out', 'wb') as f:
+        pickle.dump(vocab, f, -1)
+
+    print('complete!')
